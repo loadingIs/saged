c9b6a8127b5c8e0e941a24b6da66f22ccfc0ed91
loadingIs <104616409+loadingIs@users.noreply.github.com>
2022-04-29 13:19:54 +0200

    Update js1.js

--- js1.js
+++ js1.js
@@ -2,3 +2,5 @@ sagecell.makeSagecell({
         inputLocation: '#mycell',
         template: sagecell.templates.minimal,
         evalButtonText: 'Vykresli'});
+sagecell.makeSagecell({inputLocation: 'div.compute',
+                           evalButtonText: 'Evaluate'});
