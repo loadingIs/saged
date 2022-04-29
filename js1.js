82e4899992e065bc84d9f6044aa7559964e765fc
loadingIs <104616409+loadingIs@users.noreply.github.com>
2022-04-29 13:26:23 +0200

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,6 +1,7 @@
-sagecell.makeSagecell({
-        inputLocation: '#mycell',
-        template: sagecell.templates.minimal,
-        evalButtonText: 'Vykresli'});
-sagecell.makeSagecell({inputLocation: 'div.compute',
+    // Make the div with id 'mycell' a Sage cell
+    sagecell.makeSagecell({inputLocation:  '#mycell',
+                           template:       sagecell.templates.minimal,
+                           evalButtonText: 'Activate'});
+    // Make *any* div with class 'compute' a Sage cell
+    sagecell.makeSagecell({inputLocation: 'div.compute',
                            evalButtonText: 'Evaluate'});
